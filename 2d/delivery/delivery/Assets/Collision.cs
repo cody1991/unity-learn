87fb142891efb4631764be48e3ed7f5cf4664514
codytang <codytang@tencent.com>
2026-06-14 18:03:31 +0200

    Add Collision script to handle 2D collisions and log interactions in Unity 2D demo project. Update SampleScene to include new Collision component for enhanced gameplay dynamics.

---
+++ 2d/delivery/delivery/Assets/Collision.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class Collision : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D other) {
+        Debug.Log("Collision with " + other.gameObject.name);
+    }
+}
