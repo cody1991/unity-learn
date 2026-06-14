e40d5f6a23a5179d2651018f3061823728f04864
codytang <codytang@tencent.com>
2026-06-14 16:43:24 +0200

    Refactor Driver script to remove rotation and implement translation for smoother movement. Update SampleScene to reset local rotation and Euler angles for consistency in Unity 2D demo project.

--- 2d/delivery/delivery/Assets/Driver.cs
+++ 2d/delivery/delivery/Assets/Driver.cs
@@ -5,12 +5,13 @@ public class Driver : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        transform.Rotate(0, 0, 45);
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // transform.Rotate(0, 0, 0.02f);
+        transform.Translate(0, 0.00001f, 0);
     }
 }
