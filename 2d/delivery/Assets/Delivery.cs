1ca344e3d9b42d0b45cdd6ab9ed2494ea23dda50
codytang <codytang@tencent.com>
2026-06-19 00:41:50 +0200

    Refactor collision handling by removing the Collision class and introducing a new Delivery class to manage package interactions. Update SampleScene and TagManager to include the new 'Customer' tag for improved organization in the Unity 2D project.

---
+++ 2d/delivery/Assets/Delivery.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class Delivery : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D other) {
+        // Debug.Log("Collision with " + other.gameObject.name + " " + other.gameObject.tag);
+    }
+
+    void OnTriggerEnter2D(Collider2D other) {
+        // Debug.Log("Trigger with " + other.gameObject.name + " " + other.gameObject.tag);
+        if (other.CompareTag("Package")) {
+            Debug.Log("Package picked up"); 
+        }
+        if (other.CompareTag("Customer")) {
+            Debug.Log("Package delivered");
+        }
+    }
+}
+ 
