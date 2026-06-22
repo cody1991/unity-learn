a8c25ac281a86a4110791945077d34ce92d9e42f
codytang <codytang@tencent.com>
2026-06-22 22:10:57 +0200

    Add player controller script and associated metadata to enhance player movement mechanics. Update SampleScene to include new player controller component and adjust Rigidbody2D properties for improved gameplay dynamics.

---
+++ 2d/surfer/Assets/scripts/player_controller.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class player_controller : MonoBehaviour
+{
+    [SerializeField] float torqueAmount = 100f;
+    InputAction moveAction;
+
+    Rigidbody2D rb;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        moveAction = InputSystem.actions.FindAction("Move");
+
+        rb = GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 moveVector;
+        moveVector = moveAction.ReadValue<Vector2>();
+
+        Debug.Log(moveVector);
+
+        rb.AddTorque(-moveVector.x * torqueAmount); 
+    }
+}
