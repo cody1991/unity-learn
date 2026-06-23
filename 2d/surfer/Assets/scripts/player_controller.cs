d0d8049c1bf8f2a5204d38bbe4f0dc6c163b45e3
codytang <codytang@global.tencent.com>
2026-06-23 11:26:17 +0200

    Refactor player_controller script to introduce movement and boosting mechanics. Add base and boost speed settings, and implement player rotation and speed adjustment based on input. Enhance interaction with SurfaceEffector2D for improved gameplay dynamics.

--- 2d/surfer/Assets/scripts/player_controller.cs
+++ 2d/surfer/Assets/scripts/player_controller.cs
@@ -3,22 +3,40 @@ using UnityEngine.InputSystem;
 
 public class player_controller : MonoBehaviour
 {
+    Vector2 moveVector;
     [SerializeField] float torqueAmount = 1f;
-    InputAction moveAction;
+    [SerializeField] float baseSpeed = 15f;
+    [SerializeField] float boostSpeed = 20f;
 
+    SurfaceEffector2D surfaceEffector2D;
+
+    InputAction moveAction;
     Rigidbody2D rb;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
     void Start()
     {
         moveAction = InputSystem.actions.FindAction("Move");
         rb = GetComponent<Rigidbody2D>();
+
+        surfaceEffector2D = FindObjectOfType<SurfaceEffector2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        Vector2 moveVector;
+       rotatePlayer();
+       boostPlayer();
+    }
+
+    void rotatePlayer() {
         moveVector = moveAction.ReadValue<Vector2>();
         rb.AddTorque(-moveVector.x * torqueAmount * Time.deltaTime); 
+    }
+
+    void boostPlayer() {
+        if (moveVector.y > 0) {
+            surfaceEffector2D.speed = boostSpeed;
+        } else {
+            surfaceEffector2D.speed = baseSpeed;
+        }
     }
 }
