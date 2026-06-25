0de3c6b02c877d28c81b6275c952e14ce6816782
codytang <codytang@global.tencent.com>
2026-06-25 13:42:28 +0200

    Add player movement script and associated metadata for improved character control. Introduce PlayerMove class to handle player input and movement dynamics. Update player prefab to include new components for enhanced functionality.

---
+++ 2d/terraria/Assets/scripts/PlayerMove.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMove : MonoBehaviour
+{
+
+
+    Vector2 moveInput;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void onMove(InputValue value) {
+        moveInput = value.Get<Vector2>();
+        Debug.Log(moveInput);
+    }
+}
