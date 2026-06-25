5a2bec35cd36120c15760669458eef0ce9c29f36
codytang <codytang@global.tencent.com>
2026-06-25 14:18:44 +0200

    Update PlayerMove script to include animator integration for running state and adjust SampleScene to reflect changes in player prefab. Modify tilemap properties in SampleScene for improved gameplay dynamics.

--- 2d/terraria/Assets/scripts/PlayerMove.cs
+++ 2d/terraria/Assets/scripts/PlayerMove.cs
@@ -8,10 +8,13 @@ public class PlayerMove : MonoBehaviour
     Rigidbody2D myRigidbody;
     Vector2 moveInput;
 
+    Animator myAnimator;
+
     void Start()
     {
         // 获取 Rigidbody2D 组件
         myRigidbody = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -29,10 +32,13 @@ public class PlayerMove : MonoBehaviour
         // y 不控制，按照 rigidbody 的 y 速度
         Vector2 playerVelocity = new Vector2(moveInput.x * runSpeed, myRigidbody.linearVelocity.y);
         myRigidbody.linearVelocity = playerVelocity;
+
+        bool isRunning = Mathf.Abs(myRigidbody.linearVelocity.x) > Mathf.Epsilon;
+
+        myAnimator.SetBool("isRunning", isRunning);
     }
 
     void FlipSprite() {
-
         bool playerHasHorizontalSpeed = Mathf.Abs(myRigidbody.linearVelocity.x) > Mathf.Epsilon; // 是否存在水平速度
         if (playerHasHorizontalSpeed) {
             transform.localScale = new Vector2(Mathf.Sign(myRigidbody.linearVelocity.x), 1f);
