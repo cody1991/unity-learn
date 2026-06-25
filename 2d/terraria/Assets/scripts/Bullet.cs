7acbabf5413b83afa0cff9959bb30fa079c5b734
codytang <codytang@global.tencent.com>
2026-06-25 23:37:24 +0200

    Add bullet prefab components and script; update enemy prefab tag and properties for collision detection. Modify player prefab to include gun GameObject and adjust player movement settings. Update TagManager to include 'enemy' tag for gameplay functionality.

---
+++ 2d/terraria/Assets/scripts/Bullet.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+   Rigidbody2D myRigidbody;
+   [SerializeField] float bulletSpeed = 20f;
+
+   PlayerMove player;
+   float xSpeed;
+
+   void Start() {
+    myRigidbody = GetComponent<Rigidbody2D>();
+    player = FindFirstObjectByType<PlayerMove>();
+    xSpeed = player.transform.localScale.x * bulletSpeed;
+   }
+
+   void OnCollisionEnter2D(Collision2D other) {
+    Destroy(gameObject);
+   }
+
+   void Update() {
+
+     myRigidbody.linearVelocity = new Vector2(xSpeed, 0f);
+   }
+
+   void OnTriggerEnter2D(Collider2D other) {
+    if (other.CompareTag("enemy")) {
+        Destroy(other.gameObject);
+    }
+    Destroy(gameObject); 
+   }
+}
