3b23434d52c049b8c66fe9f183643005857d2663
codytang <codytang@global.tencent.com>
2026-06-25 13:58:15 +0200

    Enhance player movement dynamics by updating PlayerMove script to include run speed functionality and Rigidbody2D integration. Adjust SampleScene to reflect new player prefab modifications, improving character control and gameplay experience.

--- 2d/terraria/Assets/scripts/PlayerMove.cs
+++ 2d/terraria/Assets/scripts/PlayerMove.cs
@@ -4,22 +4,29 @@ using UnityEngine.InputSystem;
 public class PlayerMove : MonoBehaviour
 {
 
-
+    [SerializeField] float runSpeed = 10f;
+    Rigidbody2D myRigidbody;
     Vector2 moveInput;
 
     void Start()
     {
-        
+        // 获取 Rigidbody2D 组件
+        myRigidbody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Run();
     }
 
     void OnMove(InputValue value) {
         moveInput = value.Get<Vector2>();
-        Debug.Log(moveInput);
+    }
+
+    void Run() {
+        // y 不控制，按照 rigidbody 的 y 速度
+        Vector2 playerVelocity = new Vector2(moveInput.x * runSpeed, myRigidbody.linearVelocity.y);
+        myRigidbody.linearVelocity = playerVelocity;
     }
 }
