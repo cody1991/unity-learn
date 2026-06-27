3c19868d77d706a2d47118c704efcc26c0123695
codytang <codytang@global.tencent.com>
2026-06-27 17:28:43 +0200

    Refactor CoinPickUp script to use trigger detection instead of collision; update SampleScene to include new GameObject properties and adjust layer collision matrix for improved interactions.

--- 2d/terraria/Assets/scripts/CoinPickUp.cs
+++ 2d/terraria/Assets/scripts/CoinPickUp.cs
@@ -2,9 +2,10 @@ using UnityEngine;
 
 public class CoinPickUp : MonoBehaviour
 {
-    void OnCollisionEnter2D(Collision2D other) {
-        if (other.gameObject.layer == LayerMask.NameToLayer("Player")) {
+    void OnTriggerEnter2D(Collider2D other) {
+        if (other.CompareTag("Player")) {
             Destroy(gameObject);
         }
     }
 }
+ 
