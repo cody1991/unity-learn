b40a1f9891ce7363d22386b9fb9236cceab98f9e
codytang <codytang@global.tencent.com>
2026-06-27 17:47:06 +0200

    Add coin pickup sound effects and update CoinPickUp script to play sound on collection; create new sound assets and adjust prefab properties in SampleScene for enhanced audio feedback during gameplay.

--- 2d/terraria/Assets/scripts/CoinPickUp.cs
+++ 2d/terraria/Assets/scripts/CoinPickUp.cs
@@ -2,9 +2,20 @@ using UnityEngine;
 
 public class CoinPickUp : MonoBehaviour
 {
+
+    [SerializeField] AudioClip coinPickUpSFX;
+
+
+    bool wasCollected = false;
     void OnTriggerEnter2D(Collider2D other) {
-        if (other.CompareTag("Player")) {
+        if (other.CompareTag("Player") && !wasCollected) {
+            wasCollected = true;
+                        // playclipatpoint的意思是在当前场景中播放音频，而不是在全局播放
+            AudioSource.PlayClipAtPoint(coinPickUpSFX, transform.position);
+            gameObject.SetActive(false);
             Destroy(gameObject);
+
+
         }
     }
 }
