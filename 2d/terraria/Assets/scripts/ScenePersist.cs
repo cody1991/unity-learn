b77310b8b05e22dc080eaa3d9534b30be88bd2a7
codytang <codytang@global.tencent.com>
2026-06-29 17:33:58 +0200

    Add Awake method to ScenePersist for managing multiple instances; log the number of ScenePersist objects and ensure only one persists across scenes.

--- 2d/terraria/Assets/scripts/ScenePersist.cs
+++ 2d/terraria/Assets/scripts/ScenePersist.cs
@@ -2,6 +2,20 @@ using UnityEngine;
 
 public class ScenePersist : MonoBehaviour
 {
+    void Awake() {
+
+        int numberScenePersists = FindObjectsByType<ScenePersist>(FindObjectsSortMode.None).Length;
+
+        Debug.Log("Number of scene persists: " + numberScenePersists);
+
+        if (numberScenePersists > 1) {
+            Destroy(gameObject);
+        } else {
+            DontDestroyOnLoad(gameObject);
+        }
+
+    }
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
