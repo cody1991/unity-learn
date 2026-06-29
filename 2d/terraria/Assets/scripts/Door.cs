de62c8fb53b320f639d1c541eb803ad98efb7aec
codytang <codytang@global.tencent.com>
2026-06-29 15:14:28 +0200

    Adjust player position values in Level 1 scene for improved alignment; update Door script to introduce a load delay before scene transitions, enhancing player experience during scene changes.

--- 2d/terraria/Assets/scripts/Door.cs
+++ 2d/terraria/Assets/scripts/Door.cs
@@ -1,17 +1,21 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.Collections;
 
 public class Door : MonoBehaviour
 {
+
+    [SerializeField] float loadDelay = 1f;
     void OnTriggerEnter2D(Collider2D other) {
         PlayerMove player = other.GetComponent<PlayerMove>();
-        if (player == null) {
-            return;
-        }
+        StartCoroutine(LoadNextScene(player));
+    }
 
-        // 获取当前场景索引
+    IEnumerator LoadNextScene(PlayerMove player) {
+        // 开启协程后，会等待1秒后执行下面的代码
+        yield return new WaitForSecondsRealtime(loadDelay);
+         // 获取当前场景索引
         int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
-
         // 加载下一个场景，但是如果当前场景是最后一个场景，就不做任何操作
         if (currentSceneIndex < SceneManager.sceneCountInBuildSettings - 1) {
             player.ClearSave();
